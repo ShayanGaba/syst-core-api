7e64a7c75d977a57e328ab36761929d1d110f22f
Shayan Gaba <451shayan@gmail.com>
2026-06-19 16:45:43 +0500

    fix: enable cors origins for vercel production domains

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'https://syst-core-ui.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
